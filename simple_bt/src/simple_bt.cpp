6f77e093b5c2abe24cd9b39add3b9e75784d7636
yurivwarmerdam <yurivwarmerdam@gmail.com>
2025-03-01 16:27:43 +0100

    closed to compiling simple_bt, and removed pycache from index

--- simple_bt/src/simple_bt.cpp
+++ simple_bt/src/simple_bt.cpp
@@ -9,36 +9,35 @@
 
 namespace py = pybind11;
 
-struct SkeletonActions {
-  py::function wait;
-  py::function pick_player_walk_goal;
-  py::function walk_toward_goal;
-};
-
-BT::NodeStatus ApproachObject::tick() {
+BT::NodeStatus ApproachObject::tick()
+{
   std::cout << "approach object: " << this->name() << std::endl;
 
   std::this_thread::sleep_for(5s);
   return BT::NodeStatus::SUCCESS;
 }
 
-BT::NodeStatus CheckBattery() {
+BT::NodeStatus CheckBattery()
+{
   std::cout << "Battery OK" << std::endl;
   return BT::NodeStatus::SUCCESS;
 }
 
-BT::NodeStatus GripperInterface::open() {
+BT::NodeStatus GripperInterface::open()
+{
   std::cout << "open" << std::endl;
   _open = true;
   return BT::NodeStatus::SUCCESS;
 }
-BT::NodeStatus GripperInterface::close() {
+BT::NodeStatus GripperInterface::close()
+{
   std::cout << "close" << std::endl;
   _open = false;
   return BT::NodeStatus::SUCCESS;
 }
 
-int simple_run() {
+int simple_run()
+{
   std::cout << "testing" << std::endl;
   // Fairly sure this is an overview of different ways in wich you can
   // call/organize/define your nodes/actions.
@@ -59,7 +58,8 @@ int simple_run() {
   return 0;
 }
 
-int test_func(py::function thing) {
+int test_func(py::function thing)
+{
   thing();
   py::gil_scoped_release release;
   std::cout << "starting" << std::endl;
@@ -69,52 +69,60 @@ int test_func(py::function thing) {
   return 0;
 }
 
-class SkeletonAI {
-  SkeletonActions actions;
-  BT::BehaviorTreeFactory factory;
-
-public:
-  SkeletonAI(py::function wait, py::function pick_player_walk_goal,
-             py::function walk_toward_goal) {
-    actions.wait = wait;
-    actions.pick_player_walk_goal =
-        pick_player_walk_goal actions.walk_toward_goal = walk_toward_goal;
-    factory.registerSimpleAction("Wait", std::bind(wait))
-  }
-
-  BT::NodeStatus wait() {
-    bool result = actions.wait();
-    if (result) {
-      return BT::NodeStatus::SUCCESS;
-    } else {
-      return BT::NodeStatus::FAILURE;
-    }
-  }
-
-  BT::NodeStatus pick_player_walk_goal() {
-    bool result = actions.pick_player_walk_goal();
-    if (result) {
-      return BT::NodeStatus::SUCCESS;
-    } else {
-      return BT::NodeStatus::FAILURE;
-    }
-  }
-
-  BT::NodeStatus walk_toward_goal() {
-    bool result = actions.walk_toward_goal();
-    if (result) {
-      return BT::NodeStatus::SUCCESS;
-    } else {
-      return BT::NodeStatus::FAILURE;
-    }
-  }
-};
-
 // this does... nothing? Because it's not referenced in the cmake.
 PYBIND11_MODULE(bind, handle) { handle.def("asd", &test_func); }
 
-PYBIND11_MODULE(simple_run_bind, handle) {
+PYBIND11_MODULE(simple_run_bind, handle)
+{
   handle.doc() = "some docstring";
   handle.def("a_func", &simple_run);
   handle.def("test_func", &test_func);
+}
+
+SkeletonAI::SkeletonAI(py::function wait, py::function pick_player_walk_goal,
+                       py::function walk_toward_goal)
+{
+  actions.wait = wait;
+  actions.pick_player_walk_goal = pick_player_walk_goal;
+  actions.walk_toward_goal = walk_toward_goal;
+  factory.registerSimpleAction("Wait", std::bind(&SkeletonAI::c_wait, *this));
+}
+
+BT::NodeStatus SkeletonAI::c_wait()
+{
+  bool result = bool(actions.wait());
+  if (result)
+  {
+    return BT::NodeStatus::SUCCESS;
+  }
+  else
+  {
+    return BT::NodeStatus::FAILURE;
+  }
+}
+
+BT::NodeStatus SkeletonAI::pick_player_walk_goal()
+{
+  bool result = actions.pick_player_walk_goal();
+  if (result)
+  {
+    return BT::NodeStatus::SUCCESS;
+  }
+  else
+  {
+    return BT::NodeStatus::FAILURE;
+  }
+}
+
+BT::NodeStatus SkeletonAI::walk_toward_goal()
+{
+  bool result = actions.walk_toward_goal();
+  if (result)
+  {
+    return BT::NodeStatus::SUCCESS;
+  }
+  else
+  {
+    return BT::NodeStatus::FAILURE;
+  }
 }
--- simple_bt/src/simple_bt.h
+++ simple_bt/src/simple_bt.h
@@ -3,18 +3,27 @@
 #include <behaviortree_cpp/basic_types.h>
 #include <chrono>
 #include <iostream>
+#include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
+#include <pybind11/stl.h>
+
+
+
+namespace py = pybind11;
 
 using namespace std::chrono_literals;
 using std::string;
 
-class ApproachObject : public BT::SyncActionNode {
+class ApproachObject : public BT::SyncActionNode
+{
 public:
   explicit ApproachObject(const string &name) : BT::SyncActionNode(name, {}) {}
   BT::NodeStatus tick() override;
 };
 BT::NodeStatus CheckBattery();
 
-class GripperInterface {
+class GripperInterface
+{
 public:
   GripperInterface() : _open(true) {}
 
@@ -26,3 +35,27 @@ private:
 };
 
 int simple_run();
+
+struct SkeletonActions
+{
+  py::function wait;
+  py::function pick_player_walk_goal;
+  py::function walk_toward_goal;
+};
+
+class SkeletonAI
+{
+private:
+  SkeletonActions actions;
+  BT::BehaviorTreeFactory factory;
+
+public:
+  SkeletonAI(py::function wait, py::function pick_player_walk_goal,
+             py::function walk_toward_goal);
+
+  BT::NodeStatus c_wait();
+
+  BT::NodeStatus pick_player_walk_goal();
+
+  BT::NodeStatus walk_toward_goal();
+};
