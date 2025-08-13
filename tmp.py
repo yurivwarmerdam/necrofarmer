2c0c3a0d97185d18f1d11260bf6a30c21fda4e85
yurivwarmerdam <yurivwarmerdam@gmail.com>
2025-08-13 19:06:09 +0200

    movement is happening

--- tmp.py
+++ tmp.py
@@ -9,36 +9,55 @@ from random import randint
 
 
 class AnimationSequence:
-    def __init__(self, sequence: list[Surface],frame_time=100) -> None:
-        self.sequence = sequence
+    def __init__(self, *frames: Surface, frame_time=200) -> None:
+        self.frames = frames
+        self.frame_time = frame_time
         self.reset()
 
     def play(self):
-        pass
+        self.playing = True
+        # TODO... now what?
 
     def reset(self):
-        self.active_sprite = 0
+        self.current_frame_time = 0
+        self.idx = 0
+
+    @property
+    def image(self):
+        return self.frames[self.idx]
+
+    def tick(self, delta):
+        self.current_frame_time += delta
+        if self.current_frame_time > self.frame_time:
+            self.current_frame_time = self.current_frame_time % self.frame_time
+            self.idx = (self.idx + 1) % len(self.frames)
+            # print(self.idx)
+            return self.image
 
 
 class AnimatedSprite(Sprite):
     def __init__(
         self,
-        images: list[AnimationSequence],
+        animations: dict[str, AnimationSequence],
         pos: Vector2,
         *groups,
     ) -> None:
         super().__init__(*groups)
-        self.active_sprite = 0
-        self.active_animation = next(iter(animations.items()))
-        self.images = images
-        self.image = images[self.active_sprite]
+        self.animations = animations
+        self.active_animation = next(iter(animations.values()))
+        self.image = self.active_animation.image
         self.rect = self.image.get_rect()
         self.pos = pos
 
-    def set_image(self, image_idx):
-        self.active_sprite = image_idx
-        self.image = self.images[image_idx]
+    def set_animation(self, name):
+        self.active_animation = self.animations[name]
+        self.image = self.active_animation.image
         pass
+
+    def update(self, delta) -> None:
+        result = self.active_animation.tick(delta)
+        if result:
+            self.image = result
 
 
 pg.init()
@@ -53,16 +72,41 @@ anim_layer = Group()
 
 # animated sprite
 images_d = sheet_to_sprites(load_image("art/tardigrade.png"), Vector2(80, 80))
-print(images_d)
 
-AnimationSequence(
-    images_d[(0, 0)], images_d[(1, 0)], images_d[(2, 0)], images_d[(3, 0)]
+seq0 = AnimationSequence(
+    images_d[(0, 0)],
+    images_d[(1, 0)],
+    images_d[(2, 0)],
+    images_d[(3, 0)],
 )
-images_l = list(images_d.values())
-sprite = AnimatedSprite(images_l, Vector2(100, 100), anim_layer)
+seq1 = AnimationSequence(
+    images_d[(0, 1)],
+    images_d[(1, 1)],
+    images_d[(2, 1)],
+    images_d[(3, 1)],
+)
+seq2 = AnimationSequence(
+    images_d[(0, 2)],
+    images_d[(1, 2)],
+    images_d[(2, 2)],
+    images_d[(3, 2)],
+)
+seq3 = AnimationSequence(
+    images_d[(0, 3)],
+    images_d[(1, 3)],
+    images_d[(2, 3)],
+    images_d[(3, 3)],
+)
+
+sprite = AnimatedSprite(
+    {"0": seq0, "1": seq1, "2": seq2, "3": seq3}, Vector2(100, 100), anim_layer
+)
+
+clock = pg.time.Clock()
 
 # ---- ticking ----
 while True:
+    _delta = clock.get_time()
     # --- event loop ---
     for event in pg.event.get():
         if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_F8):
@@ -76,7 +120,13 @@ while True:
 
             print(f"click: {mouse_pos} : {tile} : [{floor(tile.x)},{floor(tile.y)}]")
 
-    sprite.set_image(randint(0, len(sprite.images) - 1))
+    # sprite.set_animation(randint(0, len(sprite.animations) - 1))
+
+    if randint(0,100)>99:
+        
+
+    # --- update loop ---
+    anim_layer.update(_delta)
 
     # --- render loop ---
     display.fill("darkblue")
