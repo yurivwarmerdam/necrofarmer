29833b77dca1e41b5e6cf191ae736dedd688ef43
Yuri van Warmerdam <yurivwarmerdam@gmail.com>
2025-09-17 23:39:42 +0200

    stuff

--- tilemaps/Slice 2.tsx
+++ tilemaps/Slice 2.tsx
@@ -2,4 +2,9 @@
 <tileset version="1.10" tiledversion="1.11.1" name="Slice 2" tilewidth="50" tileheight="49" tilecount="1" columns="1">
  <tileoffset x="-1" y="1"/>
  <image source="../art/Slice 2.png" trans="000000" width="50" height="49"/>
+ <tile id="0">
+  <properties>
+   <property name="bigtile" value="[[0,0],[0,-1]]"/>
+  </properties>
+ </tile>
 </tileset>
--- tilemaps/Slice 3.tsx
+++ tilemaps/Slice 3.tsx
@@ -2,4 +2,9 @@
 <tileset version="1.10" tiledversion="1.11.1" name="Slice 3" tilewidth="50" tileheight="53" tilecount="1" columns="1">
  <tileoffset x="-1" y="9"/>
  <image source="../art/Slice 3.png" trans="000000" width="50" height="53"/>
+ <tile id="0">
+  <properties>
+   <property name="bigtile" value="[[0,0],[1,0]]"/>
+  </properties>
+ </tile>
 </tileset>
