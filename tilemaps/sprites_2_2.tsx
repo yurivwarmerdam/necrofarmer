2d090032a74ca53003030cf96da059ca25880be5
Yuri van Warmerdam <yuri.van.warmerdam@alliander.com>
2025-09-25 16:37:06 +0200

    goop gorp
    Signed-off-by: Yuri van Warmerdam <yuri.van.warmerdam@alliander.com>

---
+++ tilemaps/sprites_2_2.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="sprites_2_2" tilewidth="66" tileheight="65" spacing="1" tilecount="14" columns="7">
+ <tileoffset x="-1" y="9"/>
+ <image source="../art/sprites_2_2.png" width="468" height="131"/>
+ <tile id="1">
+  <properties>
+   <property name="bigtile" value="[[0,0],[1,0],[0,-1],[1,-1]]"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="bigtile" value="[[0,0],[1,0],[0,-1],[1,-1]]"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="bigtile" value="[[0,0],[1,0],[0,-1],[1,-1]]"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="bigtile" value="[[0,0],[1,0],[0,-1],[1,-1]]"/>
+  </properties>
+ </tile>
+</tileset>
