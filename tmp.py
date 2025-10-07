ce1123d99ab20fb41c9d49ce113d5e8f9701b66c
Yuri van Warmerdam <yuri.van.warmerdam@alliander.com>
2025-10-07 15:52:14 +0200

    buttons just kind of... work?
    Signed-off-by: Yuri van Warmerdam <yuri.van.warmerdam@alliander.com>

--- tmp.py
+++ tmp.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame_gui
-from scripts.utils import load_image
+from scripts.utils import load_image, sheet_to_sprites
+from pygame.math import Vector2
 
 pygame.init()
 
@@ -13,15 +14,18 @@ background.fill(pygame.Color("#000000"))
 manager = pygame_gui.UIManager((800, 600))
 
 ui_image = load_image("art/tst_ui.png")
+button_sprites = sheet_to_sprites(load_image("art/thumbnails.png"), Vector2(46, 38))
+outline_sprites = sheet_to_sprites(load_image("art/outlines.png"), Vector2(54, 46))
+
 
 hello_button = pygame_gui.elements.UIButton(
-    relative_rect=ui_image.get_rect(), text="Say Hello", manager=manager
+    relative_rect=outline_sprites[(0, 0)].get_rect(), text="Say Hello", manager=manager
 )
 # image_elem = pygame_gui.elements.UIImage(hello_button.get_abs_rect(), ui_image, manager)
-hello_button.normal_images = [ui_image]
-hello_button.hovered_images = [ui_image]
-hello_button.selected_images = [ui_image]
-hello_button.disabled_images = [ui_image]
+hello_button.normal_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
+hello_button.hovered_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
+hello_button.selected_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
+hello_button.disabled_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
 hello_button.rebuild()
 
 print(hello_button.drawable_shape)
