aa2b1942378e996ce95b7f58708175b4b2882087
Yuri van Warmerdam <yurivwarmerdam@gmail.com>
2025-10-08 17:23:17 +0200

    grrr

--- tmp.py
+++ tmp.py
@@ -1,17 +1,20 @@
-import pygame
+import pygame as pg
 import pygame_gui
 from scripts.utils import load_image, sheet_to_sprites
 from pygame.math import Vector2
 
-pygame.init()
+pg.init()
 
-pygame.display.set_caption("Quick Start")
-window_surface = pygame.display.set_mode((800, 600))
+pg.display.set_caption("Quick Start")
 
-background = pygame.Surface((800, 600))
-background.fill(pygame.Color("#000000"))
+resolution = (400, 300)
 
-manager = pygame_gui.UIManager((800, 600))
+window_surface = pg.display.set_mode(resolution,pg.SCALED)
+
+background = pg.Surface(resolution)
+background.fill(pg.Color("#000000"))
+
+manager = pygame_gui.UIManager(resolution)
 
 ui_image = load_image("art/tst_ui.png")
 button_sprites = sheet_to_sprites(load_image("art/thumbnails.png"), Vector2(46, 38))
@@ -19,25 +22,28 @@ outline_sprites = sheet_to_sprites(load_image("art/outlines.png"), Vector2(54, 4
 
 
 hello_button = pygame_gui.elements.UIButton(
-    relative_rect=outline_sprites[(0, 0)].get_rect(), text="Say Hello", manager=manager
+    relative_rect=outline_sprites[(0, 0)].get_rect(), text="", manager=manager
 )
 # image_elem = pygame_gui.elements.UIImage(hello_button.get_abs_rect(), ui_image, manager)
 hello_button.normal_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
 hello_button.hovered_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
-hello_button.selected_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
-hello_button.disabled_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
+hello_button.selected_images = [outline_sprites[(1, 0)], button_sprites[(0, 0)]]
+hello_button.disabled_images = [outline_sprites[(2, 0)], button_sprites[(0, 0)]]
+
+hello_button.set_position(Vector2(60,50))
+
 hello_button.rebuild()
 
 print(hello_button.drawable_shape)
 
 
-clock = pygame.time.Clock()
+clock = pg.time.Clock()
 is_running = True
 
 while is_running:
     time_delta = clock.tick(60) / 1000.0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             is_running = False
 
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
@@ -51,4 +57,4 @@ while is_running:
     window_surface.blit(background, (0, 0))
     manager.draw_ui(window_surface)
 
-    pygame.display.update()
+    pg.display.update()
