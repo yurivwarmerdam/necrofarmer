e4be8ae8604e836369a050f68c09dd2698c85cd6
yurivwarmerdam <yurivwarmerdam@gmail.com>
2025-10-15 21:08:44 +0200

    some element progress

--- tmp.py
+++ tmp.py
@@ -1,6 +1,7 @@
-from typing import Dict
+from typing import Dict, Iterable
 import pygame as pg
 from pygame import Surface
+from pygame.constants import BUTTON_LEFT as BUTTON_LEFT
 import pygame_gui
 from pygame_gui.core import ObjectID, UIElement
 from pygame_gui.core.interfaces import (
@@ -16,8 +17,28 @@ from typing import Tuple
 from scripts.ui_shim import UIPanel
 
 
+class MyButton(pygame_gui.elements.UIButton):
+    def __init__(
+        self,
+        outline_sprites,
+        container: IContainerLikeInterface | None = None,
+    ):
+        super().__init__(
+            relative_rect=outline_sprites[(0, 0)].get_rect(),
+            text="",
+            object_id="hello_button",
+            container=container,
+        )
+        self.normal_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
+        self.hovered_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
+        self.disabled_images = [outline_sprites[(2, 0)], button_sprites[(0, 0)]]
+        self.selected_images = [outline_sprites[(1, 0)], button_sprites[(0, 0)]]
+        self.set_relative_position((10, 10))
+        self.rebuild()
+
+
 class ContextPanel(UIPanel):
-    def __init__(self, display: Surface):
+    def __init__(self, display: Surface, outline_sprites):
         screen_size = display.get_size()
         own_size = [450, 80]
         own_rect = pg.Rect(80, screen_size[1] - own_size[1], *own_size)
@@ -28,8 +49,11 @@ class ContextPanel(UIPanel):
         )
         self.image = pg.Surface(self.relative_rect.size)
         self.image.fill(pg.Color("darkgray"))
-        
-        self.buton = None
+
+        self.hello_button = MyButton(
+            outline_sprites,
+            container=self.get_container(),
+        )
 
 
 pg.init()
@@ -45,27 +69,15 @@ background.fill(pg.Color("springgreen3"))
 
 manager = pygame_gui.UIManager(resolution)
 
-ContextPanel(display)
 
 ui_image = load_image("art/tst_ui.png")
 button_sprites = sheet_to_sprites(load_image("art/thumbnails.png"), Vector2(46, 38))
 outline_sprites = sheet_to_sprites(load_image("art/outlines.png"), Vector2(54, 46))
 
+ContextPanel(display, outline_sprites)
 
-hello_button = pygame_gui.elements.UIButton(
-    relative_rect=outline_sprites[(0, 0)].get_rect(), text="", manager=manager
-)
-hello_button.normal_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
-hello_button.hovered_images = [outline_sprites[(0, 0)], button_sprites[(0, 0)]]
-hello_button.selected_images = [outline_sprites[(1, 0)], button_sprites[(0, 0)]]
-hello_button.disabled_images = [outline_sprites[(2, 0)], button_sprites[(0, 0)]]
-
-hello_button.set_position((60, 50))
-
-hello_button.rebuild()
 
 a_rect = outline_sprites[0, 0].get_rect()
-print(a_rect)
 b_rect = pg.Rect(0, 0, 54, 46)
 ui_panel = UIPanel(
     b_rect,
@@ -77,16 +89,12 @@ ui_panel = UIPanel(
 # button_layout_rect = pg.Rect(0, 0, 100, 20)
 button_layout_rect = pg.Rect(0, -30, 150, 20)
 
-shadow = UIPanel(
-    button_layout_rect,
-    manager=manager,
-    anchors={"left": "left", "bottom": "bottom"},
-)
 
 another_button = pygame_gui.elements.UIButton(
     button_layout_rect,
     text="Hello",
     manager=manager,
+    object_id="moar",
     anchors={"centerx": "centerx", "bottom": "bottom"},
 )
 # another_button.set_relative_position((0, -10))
@@ -94,7 +102,6 @@ another_button = pygame_gui.elements.UIButton(
 
 clock = pg.time.Clock()
 
-print(pg.MOUSEBUTTONUP, pygame_gui.UI_BUTTON_PRESSED)
 
 while True:
     time_delta = clock.tick(60) / 1000.0
@@ -104,7 +111,8 @@ while True:
             sys.exit()
 
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == hello_button:
+            print(event.ui_object_id)
+            if event.ui_object_id == "hello_button":
                 print("image pressed")
 
         processed = manager.process_events(event)
@@ -114,7 +122,8 @@ while True:
             pygame_gui.UI_BUTTON_PRESSED,
             pg.MOUSEBUTTONDOWN,
         ]:
-            print(processed, pg.event.event_name(event.type))
+            pass
+            # print(processed, pg.event.event_name(event.type))
 
     manager.update(time_delta)
 
