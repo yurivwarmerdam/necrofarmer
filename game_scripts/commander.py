074aedc7dea75f1d10b96d6cf74b9f12f16d2f97
Yuri van Warmerdam <yurivwarmerdam@gmail.com>
2025-11-10 00:42:58 +0100

    first commander steps

--- game_scripts/commander.py
+++ game_scripts/commander.py
@@ -1,13 +1,25 @@
 import pygame as pg
+from pygame.sprite import Sprite
+
+
+class SelectBox(Sprite):
+    
 
 class Commander():
+    """
+    Keeps track of any selected units.
+    Defers inputs to 
+    """
     def __init__(self):
         self.selected=None
+        self.dragging=False
 
     def process_events(self, event:pg.event.Event)->bool:
         is_processed = False
         if self.selected:
-            pass
+            is_processed = self.selected.process_events(event)
+        elif event is pg.MOUSEBUTTONDOWN:
+            #rect stuff with dragging
 
         # right click:
         # if selected:
---
+++ scripts/game.py
@@ -0,0 +1,23 @@
+import pygame as pg
+from abc import ABC
+
+
+class Game(ABC):
+    def __init__(self) -> None:
+        self.clock = pg.time.Clock()
+
+        pass
+
+    def run(self):
+        while True:
+            _delta = self.clock.get_time()
+            self.process_events()
+            self.update()
+
+    def process_events(self):
+        #event loop
+        pass
+
+    def update(self):
+        #update whatever needs update running
+        pass
