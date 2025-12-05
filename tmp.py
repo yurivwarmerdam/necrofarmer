397575d30abf9cef4ddee194785acb7d55413f3c
yurivwarmerdam <yurivwarmerdam@gmail.com>
2025-12-05 23:40:14 +0100

    messing wiht signals

--- tmp.py
+++ tmp.py
@@ -1,122 +1,34 @@
-from typing import Dict, Iterable
-import pygame as pg
-from pygame import Surface
-from pygame.constants import BUTTON_LEFT as BUTTON_LEFT
-import pygame_gui
-from pygame_gui.core import ObjectID, UIElement
-from pygame_gui.core.interfaces import (
-    IContainerLikeInterface,
-    IUIElementInterface,
-    IUIManagerInterface,
-)
-from scripts.utils import load_image, sheet_to_sprites
-from pygame.math import Vector2
-import sys
-from typing import Tuple
-from pygame import Rect
-from pygame.sprite import Sprite, Group
-from scripts.custom_sprites import NodeSprite
-# from scripts import camera
+from blinker import signal
 
 
-class SelectBox(NodeSprite):
+class SomeObj:
     def __init__(self) -> None:
-        super().__init__(Surface((0, 0)))
-        self.color = pg.Color(34, 135, 34)
-        self.image.fill(self.color)
-        self.image.set_alpha(128)
-        self.dragging: bool = False
-        self.start = Vector2()
-        # self.camera = camera.get_camera()
+        self.ngu = signal("number go up")
 
-    def start_drag(self):
-        self.start = pg.mouse.get_pos()
-        self.dragging = True
-
-    def stop_drag(self):
-        self.image = pg.Surface((0, 0))
-        self.dragging = False
-
-    def update(self, _delta=0.0):
-        if self.dragging:
-            mouse = pg.mouse.get_pos()
-            tl = (min(self.start[0], mouse[0]), min(self.start[1], mouse[1]))
-            size = (
-                abs(self.start[0] - pg.mouse.get_pos()[0]),
-                abs(self.start[1] - pg.mouse.get_pos()[1]),
-            )
-            self.image = Surface(size)
-            self.image.set_alpha(128)
-            self.rect = self.image.get_rect()
-            self.pos = tl
-            self.image.fill(self.color)
-            pg.draw.rect(
-                self.image, "darkgreen", self.rect.move(-self.rect.x, -self.rect.y), 1
-            )
-        else:
-            self.rect = Rect(0, 0, 0, 0)
-    def get_overlaps(self):
-        pass
+    def send(self):
+        self.ngu.send(self)
 
 
-pg.init()
+def some_func(sender):
+    print(f"gettign stuff: {sender}")
+def second_func(sender):
+    print(f"gettign more stuff: {sender}")
 
-my_group = Group()
-resolution = (636, 333)
-manager = pygame_gui.UIManager(resolution)
+some_obj = SomeObj()
+another_obj =SomeObj()
 
-display: Surface = pg.display.set_mode(resolution, pg.SCALED)
+# these two are the same object.
+ngu = signal("number go up")
+observer2=signal("number go up")
 
-background = pg.Surface(resolution)
-background.fill(pg.Color("lightblue"))
+# connecting twice, under different name does nothing the second time around.
+ngu.connect(some_func)
+observer2.connect(some_func)
 
-ui_image = load_image("art/tst_ui.png")
-button_sprites = sheet_to_sprites(load_image("art/thumbnails.png"), Vector2(46, 38))
-outline_sprites = sheet_to_sprites(load_image("art/outlines.png"), Vector2(54, 46))
-clock = pg.time.Clock()
+some_obj.send()
+# another_obj.send()
 
-# a_rect = pg.Rect(0, 0, 0, 0)
-
-# start = Rect(50, 50, 0, 0)
-
-fps_list = list(range(60))
-fps_counter = pygame_gui.elements.UITextBox("text", Rect(5, 5, 100, 30))
-
-box = SelectBox()
-box_group = Group()
-box_group.add(box)
-
-while True:
-    time_delta = clock.tick() / 1000.0
-    for event in pg.event.get():
-        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_F8):
-            pg.quit()
-            sys.exit()
-        else:
-            pass
-
-        processed = manager.process_events(event)
-
-        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-            box.start_drag()
-        elif event.type == pg.MOUSEBUTTONUP and event.button == 1:
-            box.stop_drag()
-
-    box.update()
-
-    display.blit(background, (0, 0))
-    box_group.draw(display)
-    # --- fps stuff ---
-
-    manager.update(time_delta)
-
-    fps_list.pop(0)
-    fps_list.append(time_delta)
-    asd = [1 / i for i in fps_list]
-    fps_counter.set_text(str(round(sum(asd) / len(asd))))
-
-    manager.draw_ui(display)
-
-    # --- /fps stuff ---
-
-    pg.display.update()
+# in it's simplest form, you name a signal, and either conncet, or send through it.
+# all signals wiht same name will link up.
+# It kiiinda look like multiple connects to the same func (instance?) will only fire once.
+# (think set in the observer list)
---
+++ tmp_fps.py
@@ -0,0 +1,122 @@
+from typing import Dict, Iterable
+import pygame as pg
+from pygame import Surface
+from pygame.constants import BUTTON_LEFT as BUTTON_LEFT
+import pygame_gui
+from pygame_gui.core import ObjectID, UIElement
+from pygame_gui.core.interfaces import (
+    IContainerLikeInterface,
+    IUIElementInterface,
+    IUIManagerInterface,
+)
+from scripts.utils import load_image, sheet_to_sprites
+from pygame.math import Vector2
+import sys
+from typing import Tuple
+from pygame import Rect
+from pygame.sprite import Sprite, Group
+from scripts.custom_sprites import NodeSprite
+# from scripts import camera
+
+
+class SelectBox(NodeSprite):
+    def __init__(self) -> None:
+        super().__init__(Surface((0, 0)))
+        self.color = pg.Color(34, 135, 34)
+        self.image.fill(self.color)
+        self.image.set_alpha(128)
+        self.dragging: bool = False
+        self.start = Vector2()
+        # self.camera = camera.get_camera()
+
+    def start_drag(self):
+        self.start = pg.mouse.get_pos()
+        self.dragging = True
+
+    def stop_drag(self):
+        self.image = pg.Surface((0, 0))
+        self.dragging = False
+
+    def update(self, _delta=0.0):
+        if self.dragging:
+            mouse = pg.mouse.get_pos()
+            tl = (min(self.start[0], mouse[0]), min(self.start[1], mouse[1]))
+            size = (
+                abs(self.start[0] - pg.mouse.get_pos()[0]),
+                abs(self.start[1] - pg.mouse.get_pos()[1]),
+            )
+            self.image = Surface(size)
+            self.image.set_alpha(128)
+            self.rect = self.image.get_rect()
+            self.pos = tl
+            self.image.fill(self.color)
+            pg.draw.rect(
+                self.image, "darkgreen", self.rect.move(-self.rect.x, -self.rect.y), 1
+            )
+        else:
+            self.rect = Rect(0, 0, 0, 0)
+    def get_overlaps(self):
+        pass
+
+
+pg.init()
+
+my_group = Group()
+resolution = (636, 333)
+manager = pygame_gui.UIManager(resolution)
+
+display: Surface = pg.display.set_mode(resolution, pg.SCALED)
+
+background = pg.Surface(resolution)
+background.fill(pg.Color("lightblue"))
+
+ui_image = load_image("art/tst_ui.png")
+button_sprites = sheet_to_sprites(load_image("art/thumbnails.png"), Vector2(46, 38))
+outline_sprites = sheet_to_sprites(load_image("art/outlines.png"), Vector2(54, 46))
+clock = pg.time.Clock()
+
+# a_rect = pg.Rect(0, 0, 0, 0)
+
+# start = Rect(50, 50, 0, 0)
+
+fps_list = list(range(60))
+fps_counter = pygame_gui.elements.UITextBox("text", Rect(5, 5, 100, 30))
+
+box = SelectBox()
+box_group = Group()
+box_group.add(box)
+
+while True:
+    time_delta = clock.tick() / 1000.0
+    for event in pg.event.get():
+        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_F8):
+            pg.quit()
+            sys.exit()
+        else:
+            pass
+
+        processed = manager.process_events(event)
+
+        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            box.start_drag()
+        elif event.type == pg.MOUSEBUTTONUP and event.button == 1:
+            box.stop_drag()
+
+    box.update()
+
+    display.blit(background, (0, 0))
+    box_group.draw(display)
+    # --- fps stuff ---
+
+    manager.update(time_delta)
+
+    fps_list.pop(0)
+    fps_list.append(time_delta)
+    asd = [1 / i for i in fps_list]
+    fps_counter.set_text(str(round(sum(asd) / len(asd))))
+
+    manager.draw_ui(display)
+
+    # --- /fps stuff ---
+
+    pg.display.update()
