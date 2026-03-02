71099e363d0404839ea46371bd2992816d8aaa4d
Yuri van Warmerdam <yuri.van.warmerdam@alliander.com>
2026-03-02 18:36:31 +0100

    motre
    Signed-off-by: Yuri van Warmerdam <yuri.van.warmerdam@alliander.com>

--- scripts/image_server.py
+++ scripts/image_server.py
@@ -11,34 +11,72 @@ class ImageServer:
     def __init__(self) -> None:
         # TODO: Consider loading images trough some sort of yaml.
         # Alternative idea is to load this through tiled and pytmx.
-        self.sprites = sheet_to_sprites(
+        self.tardigrade_sprites = sheet_to_sprites(
             load_image("art/tardigrade.png"), Vector2(80, 80)
         )
         self.animations = {}
 
         self.animations["tardigrade_0"] = AnimationSequence(
-            self.sprites[(0, 0)],
-            self.sprites[(1, 0)],
-            self.sprites[(2, 0)],
-            self.sprites[(3, 0)],
+            self.tardigrade_sprites[(0, 0)],
+            self.tardigrade_sprites[(1, 0)],
+            self.tardigrade_sprites[(2, 0)],
+            self.tardigrade_sprites[(3, 0)],
+            self.tardigrade_sprites[(4, 0)],
         )
         self.animations["tardigrade_1"] = AnimationSequence(
-            self.sprites[(0, 1)],
-            self.sprites[(1, 1)],
-            self.sprites[(2, 1)],
-            self.sprites[(3, 1)],
+            self.tardigrade_sprites[(0, 1)],
+            self.tardigrade_sprites[(1, 1)],
+            self.tardigrade_sprites[(2, 1)],
+            self.tardigrade_sprites[(3, 1)],
+            self.tardigrade_sprites[(4, 1)],
         )
         self.animations["tardigrade_2"] = AnimationSequence(
-            self.sprites[(0, 2)],
-            self.sprites[(1, 2)],
-            self.sprites[(2, 2)],
-            self.sprites[(3, 2)],
+            self.tardigrade_sprites[(0, 2)],
+            self.tardigrade_sprites[(1, 2)],
+            self.tardigrade_sprites[(2, 2)],
+            self.tardigrade_sprites[(3, 2)],
+            self.tardigrade_sprites[(4, 2)],
         )
         self.animations["tardigrade_3"] = AnimationSequence(
-            self.sprites[(0, 3)],
-            self.sprites[(1, 3)],
-            self.sprites[(2, 3)],
-            self.sprites[(3, 3)],
+            self.tardigrade_sprites[(0, 3)],
+            self.tardigrade_sprites[(1, 3)],
+            self.tardigrade_sprites[(2, 3)],
+            self.tardigrade_sprites[(3, 3)],
+            self.tardigrade_sprites[(4, 3)],
+        )
+        self.animations["tardigrade_4"] = AnimationSequence(
+            self.tardigrade_sprites[(0, 4)],
+            self.tardigrade_sprites[(1, 4)],
+            self.tardigrade_sprites[(2, 4)],
+            self.tardigrade_sprites[(3, 4)],
+            self.tardigrade_sprites[(4, 4)],
+        )
+        self.tardigrade_sprites = sheet_to_sprites(
+            load_image("art/simple_thopter.png"), Vector2(64, 64)
+        )
+        self.animations["thopter_0"] = AnimationSequence(
+            self.tardigrade_sprites[(0, 0)],
+            self.tardigrade_sprites[(1, 0)],
+            self.tardigrade_sprites[(2, 0)],
+            self.tardigrade_sprites[(3, 0)],
+        )
+        self.animations["thopter_1"] = AnimationSequence(
+            self.tardigrade_sprites[(0, 1)],
+            self.tardigrade_sprites[(1, 1)],
+            self.tardigrade_sprites[(2, 1)],
+            self.tardigrade_sprites[(3, 1)],
+        )
+        self.animations["thopter_2"] = AnimationSequence(
+            self.tardigrade_sprites[(0, 2)],
+            self.tardigrade_sprites[(1, 2)],
+            self.tardigrade_sprites[(2, 2)],
+            self.tardigrade_sprites[(3, 2)],
+        )
+        self.animations["thopter_3"] = AnimationSequence(
+            self.tardigrade_sprites[(0, 3)],
+            self.tardigrade_sprites[(1, 3)],
+            self.tardigrade_sprites[(2, 3)],
+            self.tardigrade_sprites[(3, 3)],
         )
 
 
