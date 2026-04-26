ef7799566e497ece6b0c7a5bbe24a7033c6fe546
Yuri van Warmerdam <yurivwarmerdam@gmail.com>
2026-04-26 16:42:52 +0200

    moving

--- game_scripts/tile_entities.py
+++ game_scripts/tile_entities.py
@@ -2,44 +2,67 @@ import pygame as pg
 from pytmx.util_pygame import load_pygame
 from pygame.math import Vector2
 from math import floor
-from scripts.tilemap import TileData
-
-class TileDataCollection:
-
-    def __init__(self, tmx_file:str) -> None:
-        self.tiles = {}
-
-        tmx_data = load_pygame(tmx_file)
-        tmx_layer = tmx_data.layers[0]  # entity_layer
-        self.data = tmx_layer.data
-        half_w = floor(tmx_data.tilewidth / 2)
-        half_h = floor(tmx_data.tileheight / 2)
-        for x, y, surf in tmx_layer.tiles():
-            pytmx_gid = self.data[y][x]
-            tileset = tmx_data.get_tileset_from_gid(pytmx_gid)
-
-            tile_properties = tmx_data.get_tile_properties_by_gid(pytmx_gid)
-            name = tile_properties["name"]
-            offset = -(Vector2(tileset.offset) + (-half_w, half_h))
-            print(pytmx_gid, type(pytmx_gid))
-            print(surf, type(surf))
-            print(tile_properties, type(tile_properties))
-            print(offset, type(offset))
-            1 / 0
-            self.tiles[name] = (
-                surf,
-                tile_properties,
-                offset,
-            )
+from scripts.tilemap import TileData, Tile, map_to_world
+from pytmx.map import TiledMap, TiledLayer
 
 
-def tranform_subtiles(tile_idxs, pos):
-    # TODO: not done.
-    return [idx + tile_idxs for idx in tile_idxs]
+class TileDataLayers:
+    def __init__(
+        self,
+        tmx_data: TiledMap | str,
+        tile_types: dict[str, type[Tile]] = {},
+    ):
+        """
+        container object for layer of TileData.
+        params:
+        tmx_data: Preloaded tmx data. If a string is supplied it is extracted, instead.
+        tile_types: optional dict of custyom tile types. Any types not recognized will default to specifying Tile.
+        """
+        if isinstance(tmx_data, str):
+            tmx_data = load_pygame(tmx_data)
+        self.tmx_data = tmx_data
+        self.tile_types = tile_types
+        self.tile_size = Vector2(tmx_data.tilewidth, tmx_data.tileheight)
+        self.isometric = self.tmx_data.orientation == "isometric"
+
+        self.layers: dict[str, dict[Vector2, TileData]] = {}
+
+        # tmx_data = load_pygame(tmx_file)
+        for source_layer in tmx_data.visible_layers:
+            if hasattr(source_layer, "data"):
+                self.layers[source_layer.name] = self.make_layerdata(source_layer)
+
+    def make_layerdata(self, source_layer: TiledLayer) -> dict[Vector2, TileData]:
+        layer = {}
+        for x, y, surf in source_layer.tiles():
+            tile = self.make_tiledata(layer, x, y, surf)
+            layer[Vector2(x, y)] = tile
+        return layer
+
+    def make_tiledata(self, tmx_layer, x, y, surf):
+        pytmx_gid = tmx_layer.data[y][x]  # Warning: y x != x y
+        tileset = self.tmx_data.get_tileset_from_gid(pytmx_gid)
+        offset = -(
+            Vector2(tileset.offset)
+            + (-floor(self.tmx_data.tilewidth / 2), floor(self.tmx_data.tileheight / 2))
+        )
+        properties: dict = self.tmx_data.get_tile_properties_by_gid(pytmx_gid) or {}
+        tile_type = self.tile_types.get(properties.get("name", ""), Tile)
+        return TileData(
+            tile_type=tile_type,
+            map_pos=Vector2(x, y),
+            world_pos=map_to_world(
+                x, y, self.tile_size.x, self.tile_size.y, self.isometric
+            ),
+            properties=properties,
+            surf=surf,
+            offset=offset,
+        )
+
 
 
 if __name__ == "__main__":
     pg.init()
     display = pg.display.set_mode((0, 0), pg.RESIZABLE)
-    tile_entities = TileDataCollection("tilemaps/tile_entities.tmx")
+    tile_entities = TileDataLayers("tilemaps/tile_entities.tmx")
     print(tile_entities.tiles)
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -2,15 +2,16 @@ from math import floor
 
 from pygame import Vector2
 from pygame.sprite import AbstractGroup, Group, LayeredUpdates
-from pytmx import TiledMap
+from pytmx.map import TiledMap, TiledLayer
 from pytmx.util_pygame import load_pygame
 
 from scripts.custom_sprites import NodeSprite
 from dataclasses import dataclass
 from pygame.surface import Surface
-from scripts.tilemap import Tile
 
 
+# lol. Allows for weird notation
+# some_tiledata.tile.type(some_tiledata)
 @dataclass
 class TileData:
     tile_type: type[Tile]
@@ -19,6 +20,7 @@ class TileData:
     properties: dict
     surf: Surface
     offset: Vector2
+    anchor: str = "bottomleft"
 
     def make_tile(self):
         return self.tile_type(
@@ -26,30 +28,87 @@ class TileData:
         )
 
 
-# lol. Weird notation
-# some_tiledata.tile.type(some_tiledata)
-
-
-class Tile:
-    def __init(self, Tiledata):
-        pass
-
-
 class Tile(NodeSprite):
-    def __init__(
-        self,
-        pos,
-        image,
-        properties: dict,
-        *groups,
-        anchor="bottomleft",
-        offset=Vector2(0, 0),
-    ):
-        super().__init__(image, pos, anchor, offset, *groups)
-        self.properties: dict = properties if properties else {}
+    def __init__(self, Tiledata: TileData, *groups):
+        super().__init__(
+            Tiledata.surf, TileData.world_pos, Tiledata.anchor, Tiledata.offset, *groups
+        )
+        self.properties = Tiledata.properties
 
     def has(self, attribute):
         return attribute in self.properties
+
+
+# class Tile(NodeSprite):
+#     def __init__(
+#         self,
+#         pos,
+#         image,
+#         properties: dict,
+#         *groups,
+#         anchor="bottomleft",
+#         offset=Vector2(0, 0),
+#     ):
+#         super().__init__(image, pos, anchor, offset, *groups)
+#         self.properties: dict = properties if properties else {}
+
+#     def has(self, attribute):
+#         return attribute in self.properties
+
+
+class TileDataLayers:
+    # TODO: maybe implement __getItem__?
+    def __init__(
+        self,
+        tmx_data: TiledMap | str,
+        tile_types: dict[str, type[Tile]] = {},
+    ):
+        """
+        container object for layer of TileData.
+        params:
+        tmx_data: Preloaded tmx data. If a string is supplied it is extracted, instead.
+        tile_types: optional dict of custyom tile types. Any types not recognized will default to specifying Tile.
+        """
+        if isinstance(tmx_data, str):
+            tmx_data = load_pygame(tmx_data)
+        self.tmx_data = tmx_data
+        self.tile_types = tile_types
+        self.tile_size = Vector2(tmx_data.tilewidth, tmx_data.tileheight)
+        self.isometric = self.tmx_data.orientation == "isometric"
+
+        self.layers: dict[str, dict[Vector2, TileData]] = {}
+
+        # tmx_data = load_pygame(tmx_file)
+        for source_layer in tmx_data.visible_layers:
+            if hasattr(source_layer, "data"):
+                self.layers[source_layer.name] = self.make_layerdata(source_layer)
+
+    def make_layerdata(self, source_layer: TiledLayer) -> dict[Vector2, TileData]:
+        layer = {}
+        for x, y, surf in source_layer.tiles():
+            tile = self.make_tiledata(layer, x, y, surf)
+            layer[Vector2(x, y)] = tile
+        return layer
+
+    def make_tiledata(self, tmx_layer, x, y, surf):
+        pytmx_gid = tmx_layer.data[y][x]  # Warning: y x != x y
+        tileset = self.tmx_data.get_tileset_from_gid(pytmx_gid)
+        offset = -(
+            Vector2(tileset.offset)
+            + (-floor(self.tmx_data.tilewidth / 2), floor(self.tmx_data.tileheight / 2))
+        )
+        properties: dict = self.tmx_data.get_tile_properties_by_gid(pytmx_gid) or {}
+        tile_type = self.tile_types.get(properties.get("name", ""), Tile)
+        return TileData(
+            tile_type=tile_type,
+            map_pos=Vector2(x, y),
+            world_pos=map_to_world(
+                x, y, self.tile_size.x, self.tile_size.y, self.isometric
+            ),
+            properties=properties,
+            surf=surf,
+            offset=offset,
+        )
 
 
 class Tilemap:
@@ -65,60 +124,62 @@ class Tilemap:
         # TODO: Map currently breaks when building a map with negative indexes. Fix this.
         self.map = {}
 
-        self.isometric = self.tmx_data.orientation == "isometric"
+        # self.isometric = self.tmx_data.orientation == "isometric"
+        tmx_data = load_pygame(tmx_file)
+        self.tile_data_layers = TileDataLayers(tmx_data)
 
-        for layer in self.tmx_data.visible_layers:
-            name = layer.name
-            self.map[name] = [
-                [None for _ in range(self.tmx_data.height)]
-                for _ in range(self.tmx_data.width)
-            ]
-            if hasattr(layer, "data"):
-                self.make_layer(layer, name)
+        for layer_name in self.tile_data_layers.layers:
+            self.init_layer(layer_name)
 
-    def make_layer(self, tmx_layer, name):
-        if tmx_layer.properties.get("LayeredUpdates", False):
-            layer = LayeredUpdates()
-        else:
-            layer = Group()
+        # for layer in self.tmx_data.visible_layers:
+        #     name = layer.name
+        #     self.map[name] = [
+        #         [None for _ in range(self.tmx_data.height)]
+        #         for _ in range(self.tmx_data.width)
+        #     ]
+        #     if hasattr(layer, "data"):
+        #         self.make_layer(layer, name)
 
-        self.layers[name] = layer
-        half_w = floor(self.tmx_data.tilewidth / 2)
-        half_h = floor(self.tmx_data.tileheight / 2)
-        for tile in tmx_layer.tiles():
-            self.make_tiledata(tmx_layer, *tile)
-            x, y, surf = tile
-            pytmx_gid = tmx_layer.data[y][x]  # Warning: y x != x y
-            tileset = self.tmx_data.get_tileset_from_gid(pytmx_gid)
-            offset = -(Vector2(tileset.offset) + (-half_w, half_h))
-            tile_properties = self.tmx_data.get_tile_properties_by_gid(pytmx_gid)
+    def init_layer(self, layer_name):
+        self.map[layer_name] = {}
+        data_layer = self.tile_data_layers.layers[layer_name]
+        for map_pos in data_layer:
+            tile_data: TileData = data_layer[map_pos]
+            tile = tile_data.tile_type(tile_data)
+            self.set_tile(tile, layer_name, tile_data.map_pos)
 
-            tile = Tile(
-                Vector2(0, 0),
-                surf,
-                tile_properties,
-                offset=offset,
-            )
+    def populate_layer(self, layere_name):
 
-            self.set_tile(tile, name, Vector2(x, y))
+        pass
 
-    def make_tiledata(self, tmx_layer, x, y, surf):
-        pytmx_gid = tmx_layer.data[y][x]  # Warning: y x != x y
-        tileset = self.tmx_data.get_tileset_from_gid(pytmx_gid)
-        offset = -(
-            Vector2(tileset.offset)
-            + (-floor(self.tmx_data.tilewidth / 2), floor(self.tmx_data.tileheight / 2))
-        )
-        return TileData(
-            Vector2(x, y),
-            self.map_to_world(x, y),
-            self.tmx_data.get_tile_properties_by_gid(pytmx_gid) or {},
-            surf,
-            offset,
-        )
+    # def make_layer(self, tmx_layer, name):
+    #     if tmx_layer.properties.get("LayeredUpdates", False):
+    #         layer = LayeredUpdates()
+    #     else:
+    #         layer = Group()
 
-    def get_layer(self, layer):
-        return self.layers[layer]
+    #     self.layers[name] = layer
+    #     half_w = floor(self.tmx_data.tilewidth / 2)
+    #     half_h = floor(self.tmx_data.tileheight / 2)
+    #     for tile in tmx_layer.tiles():
+    #         self.make_tiledata(tmx_layer, *tile)
+    #         x, y, surf = tile
+    #         pytmx_gid = tmx_layer.data[y][x]  # Warning: y x != x y
+    #         tileset = self.tmx_data.get_tileset_from_gid(pytmx_gid)
+    #         offset = -(Vector2(tileset.offset) + (-half_w, half_h))
+    #         tile_properties = self.tmx_data.get_tile_properties_by_gid(pytmx_gid)
+
+    #         tile = Tile(
+    #             Vector2(0, 0),
+    #             surf,
+    #             tile_properties,
+    #             offset=offset,
+    #         )
+
+    #         self.set_tile(tile, name, Vector2(x, y))
+
+    def get_layer(self, layer_name):
+        return self.layers[layer_name]
 
     def get_neigbors(self, tile_pos: Vector2, distance=1) -> list[Vector2]:
         result = []
@@ -144,8 +205,7 @@ class Tilemap:
 
     def set_tile(self, tile: Tile, layer: str, map_pos: Vector2):
         self.layers[layer].add(tile)
-        tile.pos = self.map_to_worldv(map_pos)
-        self.map[layer][floor(map_pos.x)][floor(map_pos.y)] = tile
+        self.map[layer][map_pos] = tile
 
     def kill_tile(self, layer: str, pos: Vector2):
         tile = self.map[layer][floor(pos.x)][floor(pos.y)]
@@ -203,3 +263,42 @@ class Tilemap:
 
     def map_to_worldv(self, map_pos: Vector2) -> Vector2:
         return self.map_to_world(floor(map_pos.x), floor(map_pos.y))
+
+
+def map_to_world(x, y, tilewidth, tileheight, isometric=False):
+    if isometric:
+        return Vector2(
+            tilewidth / 2 * (x - y),
+            tileheight / 2 * (x + y),
+        )
+    else:
+        return Vector2(
+            x * tilewidth,
+            y * tileheight,
+        )
+
+
+def map_to_worldv(map_pos: Vector2, tile_size: Vector2, isometric=False):
+    return map_to_world(map_pos.x, map_pos.y, tile_size.x, tile_size.y, isometric)
+
+
+def world_to_map(x, y, tilewidth, tileheight, isometric=False):
+    if isometric:
+        half_w = tilewidth / 2
+        half_h = tileheight / 2
+        x = (((x / half_w) + (y / half_h)) / 2) + 0.5
+        y = (((y / half_h) - (x / half_w)) / 2) + 0.5
+        return Vector2(floor(x), floor(y))
+    else:
+        return Vector2(
+            floor(x / tilewidth),
+            floor(y / tileheight),
+        )
+
+
+def world_to_mapv(world_pos: Vector2, tile_size: Vector2, isometric=False):
+    return world_to_map(world_pos.x, world_pos.y, tile_size.x, tile_size.y, isometric)
+
+
+if __name__ == "__main__":
+    Tilemap("tilemaps/another_island.tmx")
